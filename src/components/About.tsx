a4a9f16fbed378e412496baf06af810259806729
yoga-python <73431769+yoga-python@users.noreply.github.com>
2023-03-19 23:54:24 +0100

    add: about section

---
+++ src/components/About.tsx
@@ -0,0 +1,44 @@
+const About = () => {
+  return (
+    <section className="prose mt-8 text-content-text">
+      <h2 className="mb-6 text-2xl font-black text-content-text">About Me</h2>
+      <p>
+        I am a self-taught <b>full stack web developer</b> excited by building{" "}
+        <em>complex stateful web apps</em> with <b>React.js</b>, with a love for
+        the predictability of <b>TypeScript</b> and the efficiency of{" "}
+        <b>TailwindCSS</b>.
+      </p>
+      <p>
+        The ever-evolving environment of web development is exactly what my
+        dopamine receptors crave. I grasp concepts quickly and I am always eager
+        to learn new technologies and frameworks. I am a passionate problem
+        solver and I love building things just to prove my ability to. I believe
+        that limiting myself to either front-end or back-end would be limiting
+        my creativity. The most exciting applications are built with a full
+        stack approach.
+      </p>
+      <p>
+        I am <b>currently looking</b> for a full time position as a{" "}
+        <b>web developer</b>.
+      </p>
+      {/* <h3>Strengths and weaknesses</h3>
+    <ul>
+    <li>Strengths</li>
+    <ul>
+    <li>Fast learner</li>
+    <li>Self-motivated</li>
+    <li>Team player</li>
+    <li>Problem solver</li>
+    <li>Good communicator</li>
+    </ul>
+    <li>Weaknesses</li>
+    <ul>
+    <li>Can be a perfectionist</li>
+    <li>Can be a bit of a procrastinator</li>
+    </ul>
+  </ul> */}
+    </section>
+  );
+};
+
+export default About;
