07ff7148780b18464451b91051a8930e0804f265
yoga-python <73431769+yoga-python@users.noreply.github.com>
2023-03-20 12:15:22 +0100

    add: favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,10 +16,26 @@ export default function Home() {
           content="Carl Lindman web developer portfolio"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
       </Head>
       <Header />
-      <main className="flex w-screen flex-col items-center justify-start px-4 text-content-text sm:px-0">
+      <main className="flex w-full flex-col items-center justify-start px-4 text-content-text sm:px-0">
         <WebApps />
         <About />
         <Skills />
