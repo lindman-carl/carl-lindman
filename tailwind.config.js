6a1a70ac4a273340a86cbd944da0c18a1396553e
yoga-python <73431769+yoga-python@users.noreply.github.com>
2023-03-25 22:17:33 +0100

    change: content background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
           contrast: "hsla(24, 100%, 60%, 1)",
         },
         content: {
-          background: "hsla(0, 0%, 92%, 1)",
+          background: "hsla(0, 0%, 90%, 1)",
           text: "hsla(209, 100%, 18%, 1)",
           contrast: "hsla(24, 100%, 45%, 1)",
         },
